20b16a73875b85c5e0349441a0bfc1c64d6fae1a
Daniel Oluojomu <danvixent@gmail.com>
2022-03-11 18:29:54 +0100

    add if condition

--- download-certgen.sh
+++ download-certgen.sh
@@ -1,13 +1,12 @@
 OS=$(echo $1 | tr '[:upper:]' '[:lower:]')
 version=$2
 
-if [[ "$OS" == *"linux"* ]]; then
+if [ "$OS" == *"linux"* ] || [ "$OS" == *"ubuntu"* ]
+then
   echo "downloading linux version of ngrok"
   wget --output-document=certgen.tar.gz "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_linux_amd64.tar.gz"
-elif [[ "$OS" == *"mac"* ]]; then
-  echo "downloading mac version of ngrok"
-  wget --output-document=certgen.tar.gz "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_darwin_amd64.tar.gz"
-elif [[ "$OS" == *"darwin"* ]]; then
+elif [ "$OS" == *"mac"* ] || [ "$OS" == *"darwin"* ]
+then
   echo "downloading mac version of ngrok"
   wget --output-document=certgen.tar.gz "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_darwin_amd64.tar.gz"
 else
