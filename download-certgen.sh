a94a391c906053bcb35a7cbc0c45d258945aa3c7
Daniel Oluojomu <danvixent@gmail.com>
2022-03-11 19:17:52 +0100

    fix sh

--- download-certgen.sh
+++ download-certgen.sh
@@ -2,12 +2,10 @@ OS=$(echo $1 | tr '[:upper:]' '[:lower:]')
 version=$2
 outfile="certgen.tar.gz"
 
-if [ "$OS" == *"linux"* ] || [ "$OS" == *"ubuntu"* ]
-then
+if [[ "$OS" == *"linux"* ]] || [[ "$OS" == *"ubuntu"* ]]; then
   echo "downloading linux version of ngrok"
   wget --output-document=$outfile "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_linux_amd64.tar.gz"
-elif [ "$OS" == *"mac"* ] || [ "$OS" == *"darwin"* ]
-then
+elif [[ "$OS" == *"mac"* ]] || [[ "$OS" == *"darwin"* ]]; then
   echo "downloading mac version of ngrok"
   wget --output-document=$outfile "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_darwin_amd64.tar.gz"
 else
