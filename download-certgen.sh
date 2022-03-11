7903ef9d34c85a50ad4a80a40bff562d576e1ca8
Daniel Oluojomu <danvixent@gmail.com>
2022-03-11 18:31:03 +0100

    add $outfile

--- download-certgen.sh
+++ download-certgen.sh
@@ -1,14 +1,15 @@
 OS=$(echo $1 | tr '[:upper:]' '[:lower:]')
 version=$2
+outfile="certgen.tar.gz"
 
 if [ "$OS" == *"linux"* ] || [ "$OS" == *"ubuntu"* ]
 then
   echo "downloading linux version of ngrok"
-  wget --output-document=certgen.tar.gz "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_linux_amd64.tar.gz"
+  wget --output-document=$outfile "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_linux_amd64.tar.gz"
 elif [ "$OS" == *"mac"* ] || [ "$OS" == *"darwin"* ]
 then
   echo "downloading mac version of ngrok"
-  wget --output-document=certgen.tar.gz "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_darwin_amd64.tar.gz"
+  wget --output-document=$outfile "https://github.com/danvixent/certgen/releases/download/v${version}/certgen_${version}_darwin_amd64.tar.gz"
 else
     exit
 fi
